e7d3b04918a4946cddcd909e89c85384d26acdf1
contrbas@gmail.com <K4rmU78B>
2018-08-29 17:26:28 +0300

    ALL

--- srcs/main.c
+++ srcs/main.c
@@ -1,11 +1,17 @@
 #include "../includes/fdf.h"
 
-int main(void)
+//TODO: validation and parsing;
+
+void    get_contents()
+{
+    
+}
+
+int     main(void)
 {
     t_data sample;
 
     sample.data = NULL;
-
 
     printf("%s", "Makefile done");
 
