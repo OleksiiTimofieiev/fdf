efb3cc75a0872b336bc7652a121b947f6ae6345c
contrbas@gmail.com <K4rmU78B>
2018-08-29 15:55:28 +0300

    ALL

---
+++ includes/fdf.h
@@ -0,0 +1,9 @@
+#ifndef FDF_H
+# define FDF_H
+
+typedef struct s_data
+{
+    int x;
+}              t_data;
+
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+#include "../includes/fdf.h"
+
 int main(void)
 {
+    t_data sample;
+
+    sample.x = 0;
+
+
     printf("%s", "Makefile done");
 
     return (0);
