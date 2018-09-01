d9d4927cdaf4653407926c1c74f6c34067b53e96
contrbas@gmail.com <K4rmU78B>
2018-09-01 14:28:12 +0300

    ALL

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 11:33:36 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/01 13:57:44 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/01 14:28:09 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,48 @@
 
 //TODO: 1 dot; // broz to all sides;
 
-
-
-int     main(int argc, char ** argv)
+int		len_of_int_rows(int **parsed_data)
 {
-	// t_coord		**z;
+	int i;
+
+	i = 0;
+	while(parsed_data[i])
+		i++;
+	return (i);
+}
+
+void	fill_the_initial_matrix(t_coord **data, int **parsed_data)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if(!(data = (t_coord **)malloc(sizeof(t_coord*) * len_of_int_rows(parsed_data) + 1)))
+		return ;
+
+	data[len_of_int_rows(parsed_data)] = NULL;
+
+	while(data[i])
+	{
+		data[i] = (t_coord *)malloc(sizeof(t_coord) * (sizeof(parsed_data[i]) / sizeof(int)));
+		j = 0;
+		while (j < (int)(sizeof(parsed_data[i]) / sizeof(int)))
+		{
+			data[i]->x = j;
+			data[i]->y = i;
+			data[i]->z = parsed_data[i][j];
+			j++;
+		}
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	t_coord		**data;
+
+	data = NULL;
 
 	// z = (t_coord**)malloc(sizeof(t_coord*));
 
@@ -46,33 +83,34 @@ int     main(int argc, char ** argv)
 
 	// ft_putchar('\n');
 
-	int **test = NULL;
+	int **parsed_data = NULL;
 	if (argc == 2)
     {
-        test = transform_to_int(argv[1]);
-		if(!test)
+        parsed_data = transform_to_int(argv[1]);
+		if(!parsed_data)
 			ft_putstr("Invalid data in the file.\n");
 
-		int i = 0;
-		unsigned long j = 0;
+		// int i = 0;
+		// unsigned long j = 0;
 
-		while (test[i])
-		{
-			j = 0;
-			while (test[i][j] != INT_STOP)
-			{
-				ft_putnbr(test[i][j]);
-				ft_putchar(32);
+		// while (test[i])
+		// {
+		// 	j = 0;
+		// 	while (test[i][j] != INT_STOP)
+		// 	{
+		// 		ft_putnbr(test[i][j]);
+		// 		ft_putchar(32);
 
-				j++;
-			}
-			ft_putchar('\n');
-			i++;
-		}
+		// 		j++;
+		// 	}
+		// 	ft_putchar('\n');
+		// 	i++;
+		// }
 	}
+	fill_the_initial_matrix(data, parsed_data);
 
 
-	system("leaks -q fdf");
+		system("leaks -q fdf");
 
 	return (0);
 }
