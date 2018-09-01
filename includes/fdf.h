93ae16a2b7601d3b4806e4383db0e9713528a3b5
contrbas@gmail.com <K4rmU78B>
2018-09-01 11:55:27 +0300

    ALL

--- includes/fdf.h
+++ includes/fdf.h
@@ -1,20 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/01 11:55:13 by otimofie          #+#    #+#             */
+/*   Updated: 2018/09/01 11:55:24 by otimofie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FDF_H
 # define FDF_H
 # define INT_STOP 777
-#define Y_LIMIT 2000
+# define Y_LIMIT 2000
 
-#include <stdio.h>
-#include <fcntl.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
 
-#include "../libft/libft.h"
-
-typedef struct s_data
+typedef struct	s_coord
 {
-    double ** data;
-}              t_data;
+	int			x;
+	int			y;
+}				t_coord;
 
-int		validation(char **data_file);
-void	initialization(int **array, char **data_from_file, int i);
-int		**transform_to_int(char *filename);
+int				validation(char **data_file);
+void			initialization(int **array, char **data_from_file, int i);
+int				**transform_to_int(char *filename);
 
 #endif
