1a9756ed328710ea4a214018b4effe4cf5972106
contrbas@gmail.com <K4rmU78B>
2018-09-05 11:25:18 +0300

    ALL

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,36 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 11:33:36 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/05 11:19:17 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/05 11:25:14 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
 //TODO: 1 dot; // broz to all sides;
+
+void print_initial(t_coord **data)
+{
+	int i = 0;
+	int j = 0;
+
+	if (!data)
+		ft_putstr("here\n");
+	while (data[i])
+	{
+		j = 0;
+		while (data[i][j].x != INT_STOP)
+		{
+			ft_putnbr(data[i][j].x);
+			ft_putnbr(data[i][j].y);
+			ft_putnbr(data[i][j].z);
+			ft_putchar(32);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
 
 int		main(int argc, char **argv)
 {
@@ -25,6 +48,7 @@ int		main(int argc, char **argv)
 		if (!(parsed_data = transform_to_int(argv[1])))
 			ft_putstr("Invalid data in the file.\n");
 	(parsed_data) ? data = fill_the_initial_matrix(parsed_data) : exit(0);
+	print_initial(data);
 	system("leaks -q fdf");
 	return (0);
 }
