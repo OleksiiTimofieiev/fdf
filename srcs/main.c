7cd67ed10b6865c9fc13442721065e9417c9dc09
contrbas@gmail.com <K4rmU78B>
2018-09-05 11:18:14 +0300

    ALL

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 11:33:36 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/05 11:17:57 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/05 11:18:10 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,6 @@ int		main(int argc, char **argv)
 			ft_putstr("Invalid data in the file.\n");
 	}
 	(parsed_data) ? data = fill_the_initial_matrix(parsed_data) : exit(0);
-
-	int i = 0;
-	int j = 0;
-
-	if(!data)
-		ft_putstr("here\n");
-	while (data[i])
-	{
-		j = 0;
-		while (data[i][j].x != INT_STOP)
-		{
-			ft_putnbr(data[i][j].x);
-			ft_putnbr(data[i][j].y);
-			ft_putnbr(data[i][j].z);
-			ft_putchar(32);
-			j++;
-		}
-		ft_putchar('\n');
-		i++;
-	}
-	
 	system("leaks -q fdf");
 	return (0);
 }
