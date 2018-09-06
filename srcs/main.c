47281d25acc2354eefe2c4bb0a6487c5f2988bff
contrbas@gmail.com <K4rmU78B>
2018-09-06 17:24:02 +0300

    ALL

--- srcs/main.c
+++ srcs/main.c
@@ -6,27 +6,22 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 11:33:36 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/06 17:12:54 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/06 17:22:51 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/fdf.h"
-
-
-
 // done: scale, color, moves in 2d, moves in 3d;
 
-// TODO: 1 dot; // broz to all sides;
-// TODO: https://www.tutorialspoint.com/c_standard_library/math_h.htm
+#include "../includes/fdf.h"
+
+// TODO: 1 dot; // broz to all sides; // if i dot -> ups, do not want to work with it;
 // TODO: info blocks;
 // TODO: clean all possible leaks;
 // TODO: parse color;
-// TODO: leaks;
 // TODO: gradient;
 
 int main(int argc, char **argv)
 {
-	// int deal_with_keyboard_key = 0;;
 	t_g	g;
 	
 	init_g(&g);
@@ -34,19 +29,13 @@ int main(int argc, char **argv)
 		(!(g.parsed_data = transform_to_int(argv[1]))) ? ft_putstr("Invalid data in the file.\n") : 0;
 	(g.parsed_data) ? g.data = fill_the_initial_matrix(g.parsed_data) : exit(0); // TODO: func for exit;
 	
-	g.mlx_ptr = mlx_init();
-	g.win_ptr = mlx_new_window(g.mlx_ptr, MONITOR_WIDTH, MONITOR_HEIGHT, "fdf");
+	
 			
 	print(&g);
 	mlx_key_hook(g.win_ptr, deal_with_keyboard, &g);
 	mlx_loop(g.mlx_ptr);
 
-
-
-	// mlx_clear_window(mlx_ptr, win_ptr);
-	// mlx_new_window - clear(for refresh) - destroy(create bigger window),
-
+	
 	system("leaks -q fdf");
-
 	return (0);
 }
