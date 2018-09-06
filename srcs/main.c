a140742263aae1a612bf2b872fc44c4579a65ad4
contrbas@gmail.com <K4rmU78B>
2018-09-06 21:02:05 +0300

    ALL

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 11:33:36 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/06 19:46:04 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/06 20:59:09 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,19 @@ int		main(int argc, char **argv)
 {
 	t_g	g;
 
-	if (argc == 2 || argc == 3)
+	if (argc !=1)
 	{
-		init_g(&g);
-		(!(g.parsed_data = transform_to_int(argv[1]))) ?
-		ft_putstr("Invalid data in the file.\n") : 0;
+		if (argc == 2 || argc == 3)
+		{
+			init_g(&g);
+			(!(g.parsed_data = transform_to_int(argv[1]))) ?
+			ft_putstr("Invalid data in the file.\n") : 0;
+		}
+		if (argc == 3)
+			g.gradient = (!ft_strequ("-i", argv[2])) ? -2.0 : 2;
+		(g.parsed_data) ? g.data = fill_the_initial_matrix(g.parsed_data) : exit(0);
+		mlx_key_hook(g.win_ptr, deal_with_keyboard, &g);
+		mlx_loop(g.mlx_ptr);
 	}
-	if (argc == 3)
-		g.gradient = (!ft_strequ("-i", argv[2])) ? -2.0 : 2;
-	(g.parsed_data) ? g.data = fill_the_initial_matrix(g.parsed_data) : exit(0);
-	mlx_key_hook(g.win_ptr, deal_with_keyboard, &g);
-	mlx_loop(g.mlx_ptr);
 	return (0);
 }
